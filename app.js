1158ac5046e2abd547a43c86f40a103689b14f29
Gabriel Leon <gabriel.leonaveleyra@tapinfluence.com>
2016-10-31 11:47:24 -0600

    basic desired functionality done

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 var app = angular.module('damonsApp',[])
 
-app.controller('damonsController',['$scope',function ($scope) {
+//SAMPLE ENDPOINT CALLS
+// $http.get('/getMyTasks').then(function (error, response) {
+// 	if (response.status === 404) {
+// 		return stuff
+// 	}
+// 	$scope.damonsTasks = JSON.parse(response).tasks
+// })
+
+// $http.post("/url", payload).then(function (response) {
+
+	// })
+//SAMPLE ENDPOINT CALLS
+
+app.controller('damonsController',['$scope', '$http', function ($scope, $http) {
 
 	$scope.damonsTasks = []
 
 	$scope.nameOfNewTask = ""
+
+	$scope.hasDamonAddedATask = false
+
+	$scope.tempTask
+
+	$scope.newKey = ""
+
+	$scope.newValue = ""
+
+	$scope.addedAttributes = {}
+
+	$scope.damonWantsToAddATask = function (name) {
+		if (name !== "") {
+			var timeStamp = new Date()
+			$scope.tempTask = new Task(timeStamp, name)
+			$scope.nameOfNewTask = ""
+			$scope.hasDamonAddedATask = true
+		}
+	}
+
+	$scope.damonAddKeyValuePair = function (key, value) {
+		if (key !== "" && value !== "") {
+			$scope.addedAttributes[key] = value
+			$scope.newKey = ""
+			$scope.newValue = ""
+		}
+	}
 
 	var Task = function (timeOfCreation, name) {
 		this.id = ($scope.damonsTasks.length !== 0)
@@ -12,16 +52,33 @@ app.controller('damonsController',['$scope',function ($scope) {
 			: 1
 		this.timeOfCreation = timeOfCreation
 		this.name = name
+		this.display = false
 	}
 
-	$scope.createATask = function (name) {
-		var timeStamp = new Date()
-		$scope.addATask(new Task(timeStamp, name))
-		$scope.nameOfNewTask = ""
+	$scope.cancelAddTask = function () {
+		$scope.tempTask = {}
+		$scope.addedAttributes = {}
+		$scope.newKey = ""
+		$scope.newValue = ""
+		$scope.hasDamonAddedATask = false
 	}
 
 	$scope.addATask = function (task) {
+		for (key in $scope.addedAttributes) {
+			task[key] = $scope.addedAttributes[key]
+		}
 		$scope.damonsTasks.push(task)
+		$scope.hasDamonAddedATask = false
+		$scope.tempTask = {}
+		$scope.addedAttributes = {}
+	}
+
+	$scope.seeTaskDetails = function (taskId) {
+		for (var i = 0; i < $scope.damonsTasks.length; i++) {
+			if ($scope.damonsTasks[i].id === taskId) {
+				$scope.damonsTasks[i].display = !$scope.damonsTasks[i].display
+			}
+		}
 	}
 
 	$scope.deleteATask = function (taskId) {
@@ -32,5 +89,8 @@ app.controller('damonsController',['$scope',function ($scope) {
 		}
 	}
 
+	$scope.deleteKeyValuePair = function (key) {
+		delete $scope.addedAttributes[key]
+	}
 
 }])
